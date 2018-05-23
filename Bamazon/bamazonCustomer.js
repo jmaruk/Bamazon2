b3e164d7942361d3ec5f92a258779eddb06239f0
Jaylee Maruk <jmaruk@Jaylees-MacBook-Pro.local>
2018-05-22 21:52:32 -0700

    updates

--- Bamazon/bamazonCustomer.js
+++ Bamazon/bamazonCustomer.js
@@ -1,30 +1,50 @@
-var inquirer = require('inquirer');
-inquirer.prompt([{
-    type: "input", 
-    name: "Item_ID", 
-    message: "What is the ID of the product you want to buy?"}, 
-
-    {type: "input", 
-    name: "Quanity", 
-    message: "How many units of this product would you like to buy?"}]).then(answers => {
-    console.log(answers);
-    connection.query('SELECT stock_quantity FROM products WHERE item_id =' + answers.Item_ID, function (error, results, fields) {
-        if (error) throw error;
-        console.log(results);
-      });
-});
-
+var itemId = "";
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : '/',
+  user     : 'root',
   password : '',
   database : 'bamazon'
-});
+}); 
+
+var inquirer = require('inquirer');
+
+
+function questions() {
+
+  connection.query('SELECT * FROM products', function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+  });
+
+  
+  inquirer.prompt([{
+    type: "input", 
+    name: "Item_ID", 
+    message: "What is the ID of the product you want to buy?"}]).then(function(answers) {
+      itemId = answers.Item_ID;
+      inquirer.prompt([ 
+
+        {type: "input", 
+        name: "Quanity", 
+        message: "How many units of this product would you like to buy?"}]).then(function(answers) {
+        console.log(answers);
+        connection.query('SELECT stock_quantity FROM products WHERE item_id =' + itemId, function (error, results, fields) {
+            if (error) throw error;
+            console.log(results);
+          });
+          questions();
+    });
+  });
+}
+
+
+
+questions();
  
-connection.connect();
+// connection.connect();
  
 
  
-connection.end();
+// connection.end();
 
